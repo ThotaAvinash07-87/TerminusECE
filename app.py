92677992ae53b4ce98f2271f699229b861309496
Thota Avinash <thotaavinash8795@gmail.com>
2026-07-05 15:16:02 +0530

    Add header, footer, and label

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Label
 
 class MyApp(App):
-    pass
+    TITLE = "TerminusECE"
+    # The compose function tells the app what to draw on the screen
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Label("Hello, World! I am building a terminal app.")
+        yield Footer()
 
 if __name__ == "__main__":
     MyApp().run()
